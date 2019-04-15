3cdafae853a821de510bcfdeead7ae220587d673
Christopher J. Brody <chris@brody.consulting>
2019-04-15 13:23:47 -0400

    Use AppBar from sample
    in https://github.com/grommet/grommet-starter-new-app

--- src/App.js
+++ src/App.js
@@ -4,10 +4,27 @@ import { Box } from 'grommet';
 
 import { Text, View } from 'react-primitives';
 
+const AppBar = (props) => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='brand'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='medium'
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
+
 class App extends Component {
   render() {
     return (
       <View>
+        <AppBar>
+          Hello Grommet App
+        </AppBar>
         <Box>
           <h2>First part</h2>
           <Text>text component</Text>

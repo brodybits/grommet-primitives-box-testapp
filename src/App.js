a0000ac8755fbee0b72192d44d98f0b68d32f5ab
Christopher J. Brody <chris@brody.consulting>
2019-04-15 13:20:37 -0400

    Wrap first part in a Grommet Box

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 
+import { Box } from 'grommet';
+
 import { Text, View } from 'react-primitives';
 
 class App extends Component {
   render() {
     return (
       <View>
-        <h2>First part</h2>
-        <Text>text component</Text>
-        <div>div text</div>
+        <Box>
+          <h2>First part</h2>
+          <Text>text component</Text>
+          <div>div text</div>
+        </Box>
         <h2>Second part in another View</h2>
         <View>
           <Text>text component</Text>
